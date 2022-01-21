02acae1115b487e0cac3267a85eea5049d1eeade
lillyleiran <lilly.leiran@gmail.com>
2022-01-21 15:32:55 -0700

    add warning that more than 30 issues can't be displayed

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,6 +9,10 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to DOM function
                 displayIssues(data);
+                // check if api has paginated issues (more than 30)
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -47,4 +52,16 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("lillylav/git-it-done");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    // create a link element to go with warning
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on Gihub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
